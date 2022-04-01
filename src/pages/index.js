ea7aed0041e4fa553a192341fbb6657234ced5a5
Wonjang Son <sondaniel.88@gmail.com>
2022-04-01 12:03:08 +0200

    Fixed helmet og values

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,41 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../style/main.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/main.scss";
 
-import { Helmet } from 'react-helmet';
-import React from 'react';
-import App from '../components/App';
-import { headData } from '../data';
+import App from "../components/App";
+import { Helmet } from "react-helmet";
+import { headData } from "../data";
 
 export default () => {
   const { title, lang, description } = headData;
 
   return (
     <>
-      <Helmet>
+      <Helmet
+        title={title}
+        meta={[
+          {
+            property: "og:title",
+            content: title,
+          },
+          {
+            property: "og:type",
+            content: "website",
+          },
+          {
+            property: "og:description",
+            content: description,
+          },
+          {
+            property: "og:image",
+            content:
+              "https://filoscoder.netlify.app/static/c80f7757ad076ae146f5038df44646aa/89f4f/profile.jpg",
+          },
+        ]}
+      >
         <meta charSet="utf-8" />
         <title>{title}</title>
-        <html lang={lang || 'en'} />
+        <html lang={lang || "en"} />
         <meta name="description" content={description} />
-        <meta property="og:title" content={title} data-rh="true" />
-        <meta property="og:description" content={description} data-rh="true" />
-        <meta
-          property="og:image"
-          content="https://filoscoder.netlify.app/static/c80f7757ad076ae146f5038df44646aa/89f4f/profile.jpg"
-          data-rh="true"
-        />
       </Helmet>
       <App />
     </>
